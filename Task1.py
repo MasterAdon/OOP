b1e9df2ac8f271bce73cc21b0bbbe23fce8a0885
Aleksey Egorov <aleksey358358@gmail.com>
2021-04-06 14:23:13 +0300

    Lecturer grade added & tested

--- Task1.py
+++ Task1.py
@@ -11,15 +11,14 @@ class Student:
         self.finished_courses.append(course_name)
 
     def rate_lec(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress or self.finished_courses:
-           if course in lecturer.grades:
-               lecturer.grades[course] += [grade]
-           else:
-               lecturer.grades[course] = [grade]
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress or self.finished_courses:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
         else:
             return "Ошибка"
-
-
 
 
 class Mentor:
@@ -28,19 +27,6 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
-
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-
-
-# print(best_student.grades)
 
 class Lecturer:
     def __init__(self, name, surname):
@@ -61,4 +47,27 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
 
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+# print(best_student.grades)
+
+
+best_lector = Lecturer('Ivan', 'Ivanov')
+best_lector.courses_attached += ['Python']
+
+cool_student = Student('Petr', 'Petrov', 'man')
+cool_student.courses_in_progress += ['Python']
+
+cool_student.rate_lec(best_lector, 'Python', 10)
+cool_student.rate_lec(best_lector, 'Python', 8)
+
+# print(best_lector.grades)
+# Реализована взможность получения оценки для преподователей, по аналогии с оценками студентов
